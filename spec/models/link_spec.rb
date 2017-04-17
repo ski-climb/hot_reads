5a7e8cb022a7d23e63d5ddf120741556e4971c22
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-17 16:25:01 -0600

    Fix typo

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -13,7 +13,7 @@ describe Link do
     let!(:link_2) { Link.create(url: "http://www.dogs.com", read_count: 11) }
     let!(:link_3) { Link.create(url: "http://www.anteaters.com", read_count: 33) }
 
-    it "returns the links by popularity according to read_counts" do
+    it "returns the links by popularity according to read_count" do
       expect(Link.by_read_count).to match_array([link_3, link_1, link_2])
     end
   end
