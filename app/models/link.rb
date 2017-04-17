af7f3e2eef3e49656ad5cd71562798e8ad629529
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-17 15:47:33 -0600

    Validate Links have URLs

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,4 @@
 class Link < ApplicationRecord
+  validates_presence_of :url
   validates_uniqueness_of :url, case_sensitive: false
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 describe Link do
-  it { is_expected.to have_db_column(:url) }
-  it { is_expected.to validate_uniqueness_of(:url).case_insensitive }
-  it { is_expected.to have_db_column(:read_count) }
+  context "validations" do
+    it { is_expected.to validate_presence_of(:url) }
+    it { is_expected.to have_db_column(:url) }
+    it { is_expected.to validate_uniqueness_of(:url).case_insensitive }
+    it { is_expected.to have_db_column(:read_count) }
+  end
 end
