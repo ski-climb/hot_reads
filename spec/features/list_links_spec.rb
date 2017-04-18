13f8030b9a8ac25d11a0940d20f59987bc1a5e06
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-17 20:45:38 -0600

    Failing spec for listing links on home page

--- spec/features/list_links_spec.rb
+++ spec/features/list_links_spec.rb
@@ -2,14 +2,19 @@ require 'rails_helper'
 
 describe "Viewing list of links on home page" do
   context "links exist in the database" do
-    let!(:link_1) { Link.create(url: "http://www.cats.com") }
-    let!(:link_2) { Link.create(url: "http://www.dogs.com") }
+    let!(:link_1) { Link.create(url: "http://www.cats.com", read_count: 22) }
+    before do
+      10.times do |n|
+        Link.create(url: "http://www.dogs_#{n}.com", read_count: 333)
+      end
+    end
 
-    it "displays all link urls on the page" do
+    it "displays up to ten link urls on the page" do
       visit root_path
 
-      expect(page).to have_content link_1.url
-      expect(page).to have_content link_2.url
+      expect(page).to have_content "dogs_0.com"
+      expect(page).to have_content "dogs_9.com"
+      expect(page).not_to have_content link_1.url
     end
   end
 end
