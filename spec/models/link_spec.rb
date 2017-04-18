c273383b8c2376a315503253869df3d606313769
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-17 20:44:45 -0600

    Failing spec to return the most read link

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -14,7 +14,18 @@ describe Link do
     let!(:link_3) { Link.create(url: "http://www.anteaters.com", read_count: 33) }
 
     it "returns the links by popularity according to read_count" do
-      expect(Link.by_read_count).to match_array([link_3, link_1, link_2])
+      expect(Link.by_read_count.first.id).to eq link_3.id
+      expect(Link.by_read_count.last.id).to eq link_2.id
+    end
+  end
+
+  describe ".top_link_url" do
+    let!(:link_1) { Link.create(url: "http://www.cats.com", read_count: 22) }
+    let!(:link_2) { Link.create(url: "http://www.dogs.com", read_count: 11) }
+    let!(:link_3) { Link.create(url: "http://www.anteaters.com", read_count: 33) }
+
+    it "returns the url of the link with the highest read_count" do
+      expect(Link.top_link_url.first).to eq link_3.url
     end
   end
 end
