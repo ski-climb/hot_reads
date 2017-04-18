4257aa12c44fd155f0904567be3467d9258228d9
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-04-17 20:45:16 -0600

    Link model returns most read link by read_count

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,5 +2,9 @@ class Link < ApplicationRecord
   validates_presence_of :url
   validates_uniqueness_of :url, case_sensitive: false
 
-  scope :by_read_count, -> { order(:read_count) }
+  scope :by_read_count, -> { order(read_count: :desc) }
+
+  def self.top_link_url
+    Link.by_read_count.limit(1).pluck(:url)
+  end
 end
